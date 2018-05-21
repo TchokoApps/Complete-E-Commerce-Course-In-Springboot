4c41e0279864230b6fed150b7ae9d85168e2406c
Tchoko Apps <tchokoapps@gmail.com>
2018-05-21 12:02:29 +0200

    more code added

---
+++ src/main/java/com/tchoko/springboot/bookstore/ECommerceCourseInSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tchoko.springboot.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCourseInSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCourseInSpringbootApplication.class, args);
+	}
+}
